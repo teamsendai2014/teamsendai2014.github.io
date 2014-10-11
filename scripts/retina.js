9bad129c5e05b3421799322962a69dc11c45751c
teamsendai2014 <teamsendai2014@gmail.com>
2014-10-11 16:19:54 +0900

    section img => center

--- scripts/retina.js
+++ scripts/retina.js
@@ -5,7 +5,7 @@
 $(function () {
     if (window.devicePixelRatio > 1) {
         $('img').each(function() {
-            $(this).attr('src', $(this).attr('src').replace(/(\.)(png|jpg|gif)/gi,'@2x$1$2'));
+//            $(this).attr('src', $(this).attr('src').replace(/(\.)(png|jpg|gif)/gi,'@2x$1$2'));
         });
     }
 });
