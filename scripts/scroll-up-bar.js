cc8b500376a4a7c62bd0ca354b2125cc0757f209
teamsendai2014 <teamsendai2014@gmail.com>
2014-10-25 02:01:27 +0900

    takahashi

--- scripts/scroll-up-bar.js
+++ scripts/scroll-up-bar.js
@@ -12,7 +12,9 @@
         topbarHeight = $topbar.outerHeight(),
         lastY = 0, // Keep track of the last Y to detect scroll direction.
         revealing = false, // Indicate if the bar's reveal is in progress.
-        timeout;
+        timeout,
+        scrollstart=-1;
+        this.css('position','fixed');
 
     $window.scroll(function() {
       var y = $window.scrollTop();
@@ -31,30 +33,28 @@
         // The first scroll up places the bar right above the top frame.
         if (!revealing) {
           revealing = true;
-
           if (y > topbarHeight) {
-            $topbar.css('top', y - topbarHeight);
+           scrollstart=lastY - topbarHeight;
           }
         }
 
         // Scrolls up bigger than the bar's height fixes the bar on top.
-        if (parseInt($topbar.css('top')) > y) {
-          $topbar.css({
-            'position': 'fixed',
-            'top': 0
-          });
+        if (scrollstart < 0 || scrollstart > y) {
+          $topbar.css('top', 0);
+          scrollstart=-1;
+        }else{
+          $topbar.css('top', scrollstart - y);
         }
 
         // Fire an event to reveal the entire bar after 400ms if the scroll
         // wasn't big enough.
         timeout = setTimeout(function() {
-          if (y < parseInt($topbar.css('top')) + topbarHeight) {
+          if (scrollstart > 0 && scrollstart < y) {
             $topbar.css({
-              'position': 'fixed',
-              'top': parseInt($topbar.css('top')) - y
+              'top': scrollstart - y
             });
-
             $topbar.animate({'top': 0}, 100);
+            scrollstart=-1;
           }
         }, 400);
       } else { // Scrolling down
@@ -62,19 +62,18 @@
 
         // The first scroll down unfixes the bar allowing it to scroll with the
         // page.
-        if ($topbar.css('position') == 'fixed') {
-          $topbar.css({
-            'position': 'absolute',
-            'top': y
-          });
+        if (scrollstart < 0) {
+          scrollstart=lastY;
         }
+        $topbar.css('top',scrollstart - y);
 
         // Fire an event to hide the entire bar after 400ms if the scroll
         // wasn't big enough.
         timeout = setTimeout(function() {
-          if (y < parseInt($topbar.css('top')) + topbarHeight) {
+          if (y < scrollstart + topbarHeight) {
             if (y > topbarHeight) {
-              $topbar.animate({'top': y - topbarHeight}, 100);
+              $topbar.animate({'top': - topbarHeight}, 100);
+              scrollstart=lastY-topbarHeight;
             }
           }
         }, 400);
